a2d2e939dc891f6d70894e31e02d71e4ae7f10c5
ScriptSorcererMani <chrehmanali5@gmail.com>
2024-04-23 16:18:35 +0500

    Components Files are added

---
+++ src/components/Logo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo() {
+  return (
+    <h1>🌴Far Away💼</h1>
+  )
+}
+
+export default Logo 
---
+++ src/components/PackgingList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PackgingList() {
+  return (
+    <div>PackgingList</div>
+  )
+}
+
+export default PackgingList
