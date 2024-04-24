30c1224dc27198f23669a2f4157469e67d988a33
ScriptSorcererMani <chrehmanali5@gmail.com>
2024-04-24 22:23:19 +0500

    Packing List is Modified

--- src/components/PackgingList.js
+++ src/components/PackgingList.js
@@ -3,6 +3,7 @@ import Item from './Item';
 const initialItems = [
     { id: 1, description: "Passports", quantity: 2, packed: false },
     { id: 2, description: "Socks", quantity: 12, packed: true },
+    { id: 2, description: "pants", quantity: 4, packed: false },
   ];
 
 function PackgingList() {
