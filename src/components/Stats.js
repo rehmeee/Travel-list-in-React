e9d604a62177436586cb29c672c6d7811d847177
ScriptSorcererMani <chrehmanali5@gmail.com>
2024-05-02 13:31:15 +0500

    conditional rendring applied to stats

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,11 +1,19 @@
 function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Starting adding some items to your packing list 🚀</em>{" "}
+      </p>
+    );
   const itemPacked = items.filter((item) => item.packed).length;
   const percentage = Math.round((itemPacked / items.length) * 100);
   return (
     <footer className="stats">
       <em>
-        you have {items.length} items on your list, and you already packed{" "}
-        {itemPacked}({percentage}%)
+        {percentage === 100
+          ? "you got Everything, Ready to go ✈️"
+          : `you have ${items.length} items on your list, and you already packed
+        ${itemPacked}(${percentage}%)`}
       </em>
     </footer>
   );
